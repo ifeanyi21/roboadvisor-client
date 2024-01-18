8017e2e16fd3a8b05efb3598eaa7443edf0bb566
Ifeanyichukwu <85758228+ifeanyi21@users.noreply.github.com>
2024-01-18 13:44:54 +0000

    add data from api

--- src/components/RiskCalculator.js
+++ src/components/RiskCalculator.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 import Slider from "@mui/material/Slider";
 import axios from "axios";
+import { Skeleton } from "@mui/material";
 
 // const getPortfolioData = async (riskLevel) => {
 
@@ -10,7 +11,7 @@ import axios from "axios";
 function RiskCalculator() {
   const [riskLevel, setRiskLevel] = useState(3);
   const [show, setShow] = useState(true);
-  const [portfolioData, setPortfolioData] = useState("");
+  const [portfolioData, setPortfolioData] = useState({})
 
   const hanldeUpdate = (e) => {
     setRiskLevel(e.target.value);
@@ -62,9 +63,45 @@ function RiskCalculator() {
         <h2 className="text-xl">Portfolio</h2>
         <p className="my-2">Risk Level: {riskLevel}</p>
         <div>
-          <div className="flex gap-3">
+        <div className="flex gap-3 mb-4">
             <p>Nigerian Bonds</p>
-            <p>{portfolioData["Nigerian Bonds"] || ""}</p>
+            <p>{!show ? portfolioData["Nigerian Bonds"]: <Skeleton animation="wave" className="w-[300px]" />}</p>
+          </div>
+          <div className="flex gap-3 mb-4">
+            <p>Nigerian Stocks</p>
+            <p>{!show ? portfolioData["Nigerian Stocks"]: <Skeleton animation="wave" className="w-[300px]" />}</p>
+          </div>
+          <div className="flex gap-3 mb-4">
+            <p>Real Estate</p>
+            <p>{!show ? portfolioData["Real Estate"]: <Skeleton animation="wave" className="w-[300px]" />}</p>
+          </div>
+          <div className="flex gap-3 mb-4">
+            <p>T-Bills</p>
+            <p>{!show ? portfolioData["T-Bills"]: <Skeleton animation="wave" className="w-[300px]" />}</p>
+          </div>
+          <div className="flex gap-3 mb-4">
+            <p>Alternative</p>
+            <p>{!show ? portfolioData["Alternative"]: <Skeleton animation="wave" className="w-[300px]" />}</p>
+          </div>
+          <div className="flex gap-3 mb-4">
+            <p>Commodities</p>
+            <p>{!show ? portfolioData["Commodities"]: <Skeleton animation="wave" className="w-[300px]" />}</p>
+          </div>
+          <div className="flex gap-3 mb-4">
+            <p>Emerging Stocks</p>
+            <p>{!show ? portfolioData["Emerging Stocks"]: <Skeleton animation="wave" className="w-[300px]" />}</p>
+          </div>
+          <div className="flex gap-3 mb-4">
+            <p>Foriegn Stocks</p>
+            <p>{!show ? portfolioData["Foriegn Stocks"]: <Skeleton animation="wave" className="w-[300px]" />}</p>
+          </div>
+          <div className="flex gap-3 mb-4">
+            <p>Foriegn Bonds</p>
+            <p>{!show ? portfolioData["Foriegn Bonds"]: <Skeleton animation="wave" className="w-[300px]" />}</p>
+          </div>
+          <div className="flex gap-3 mb-4">
+            <p>Tech Stocks</p>
+            <p>{!show ? portfolioData["Tech Stocks"]: <Skeleton animation="wave" className="w-[300px]" />}</p>
           </div>
         </div>
       </section>
